e2b8acbc8fca896c72a89a13273fe552a31b4bf8
AdamShafei <AdamShafei@192.168.0.107>
2015-10-23 16:56:45 +0200

    nothing really

--- SE_Grp73/src/se_ex01/DotsNBoxesEngine.java
+++ SE_Grp73/src/se_ex01/DotsNBoxesEngine.java
@@ -2,7 +2,9 @@ package se_ex01;
 
 public class DotsNBoxesEngine {
 	public String[][] map;
-
+    public int widthOfMap;
+    public int heightOfMap;
+	
 	/**
 	 * Initialize the field size of the map
 	 * 
@@ -13,13 +15,21 @@ public class DotsNBoxesEngine {
 	 */
 	public DotsNBoxesEngine(int width, int height) {
 		if (width >= 2 && width % 2 == 0 && height >= 2 && height % 2 == 0) {
+			widthOfMap = width;
+			heightOfMap = height;
 			this.map = new String[width][height];
 		} else
 			System.err.println("Wrong scaling of the map");
 	}
 	
 	
-	// asdasd 
-	// adfjofjao
+	public void initializeMap(){
+		
+		
+		
+		
+		
+	}
+	
 	
 }
