5fe5bd5af85bde86aecbdfc1867723994289212f
AdamShafei <AdamShafei@192.168.0.113>
2015-10-31 12:27:43 +0100

    Neue Methoden in KIRandom() und KI

--- SE_Grp73/src/se_ex01/KI.java
+++ SE_Grp73/src/se_ex01/KI.java
@@ -1,10 +1,35 @@
 package se_ex01;
 
+import java.util.ArrayList;
+
 public abstract class KI extends Player {
+
+	ArrayList remainingNumbers = new ArrayList();
 
 	public KI(String name, int score) {
 		super(name, score);
 		// TODO Auto-generated constructor stub
+	}
+
+	public static boolean isNumber(String str) {
+		try {
+			double d = Double.parseDouble(str);
+		} catch (NumberFormatException nfe) {
+			return false;
+		}
+		return true;
+	}
+
+	public void calculateRemainingNumbers(String[][] map, int width, int height) {
+
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+
+				if (isNumber(map[i][j])) {
+					remainingNumbers.add(Integer.valueOf(map[i][j]));
+				}
+			}
+		}
 	}
 
 }
--- SE_Grp73/src/se_ex01/KIRandom.java
+++ SE_Grp73/src/se_ex01/KIRandom.java
@@ -1,10 +1,24 @@
 package se_ex01;
 
+import java.util.ArrayList;
+
 public class KIRandom extends KI {
+
+	//ArrayList remainingNumbers = new ArrayList();
 
 	public KIRandom(String name, int score) {
 		super(name, score);
 		// TODO Auto-generated constructor stub
+	}
+
+	
+	public int chooseNumber() {
+
+		int randomPosition = (int) (Math.random() * (remainingNumbers.size()-1));
+		int chosenNumber = (int) remainingNumbers.get(randomPosition);
+		remainingNumbers.remove(chosenNumber);
+		return chosenNumber;
+		
 	}
 
 }
