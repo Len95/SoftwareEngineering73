9785a7bf9a46062708ac4a6fb52f4d3e4d6f1a0e
Len Williamson <len13@hotmail.de>
2015-11-03 15:44:54 +0100

    Fehler gefunden, aber Behebung macht Probleme
    In der AIMinMaxAlgo Klasse wird in den Methoden calcPointsOpt1/2() die
    Inputwerte in den if abfragen geändert sodass bspw. in line 108 eine
    outOfBoundExcept. kommt.. 
    
    -> syso vgl.

--- SE_Grp73/src/se_ex01/AIMinMaxAlgo.java
+++ SE_Grp73/src/se_ex01/AIMinMaxAlgo.java
@@ -19,11 +19,15 @@ public class AIMinMaxAlgo extends AI {
 	public int getNextMove() {
 		minMaxAlgo(0, 0, engine.getMap());
 		int[] bestChoice = options.get(0);
+		System.out.println("options.size: " + options.size());
 		for (int[] option : options) {
+			System.out.println("Optioin[0]: " + option[0] + " Option[1]: " + option[1]);
+
 			if (option[1] >= bestChoice[1]) {
 				bestChoice = option;
 			}
 		}
+
 		return bestChoice[0];
 	}
 
@@ -43,9 +47,14 @@ public class AIMinMaxAlgo extends AI {
 
 		if (super.police.isNumeric(map[startHeight][startWidth])) {
 			// Found a wall number -> update options
+			System.out.println("WallNo.: " + map[startHeight][startWidth] + " h|w " + startHeight + "|"+ startWidth);
 			canidate[0] = Integer.valueOf(map[startHeight][startWidth]); // Wall-number
 			canidate[1] = calculatePossiblePoints(startHeight, startWidth, map); // Possible-points
 			options.add(canidate);
+			if (startWidth % 2 == 0 && startWidth == (engine.getWidth() - 1)) {
+				minMaxAlgo(++startHeight, 0, map);
+			} else
+				minMaxAlgo(startHeight, ++startWidth, map);
 		} else if (startWidth >= (engine.getWidth() - 1) && !(startHeight >= (engine.getHeight() - 1))) {
 			// Start at the beginning of the next row
 			minMaxAlgo(++startHeight, 0, map);
@@ -77,14 +86,25 @@ public class AIMinMaxAlgo extends AI {
 	private int calculatePointsOption1(int height, int width, String[][] map) {
 		int points = 0;
 
-		if (width != 0) { // we can check left field
+		// we can check left field
+		if (width != 0) {
+			
+			System.out.println("Height LeftField: " + height);
+			System.out.println("Width LeftField: " + width);
+			
 			if (map[height--][width--].equals("-") && map[height][width -= 2].equals("|")
 					&& map[height++][width--].equals("-")) {
 				points += 1;
 			} else
 				points += 0;
-
-		} else if (width != engine.getWidth()) { // we can check right field
+		}
+		
+		// we can check right field
+		if (width != (engine.getWidth() - 1)) {
+			
+			System.out.println("Height RightField: " + height);
+			System.out.println("Width RightField: " + width);
+			
 			if (map[height--][width++].equals("-") && map[height][width += 2].equals("|")
 					&& map[height++][width++].equals("-")) {
 				points += 1;
@@ -105,14 +125,17 @@ public class AIMinMaxAlgo extends AI {
 	private int calculatePointsOption2(int height, int width, String[][] map) {
 		int points = 0;
 
-		if (height != 0) { // we can check upper field
+		// we can check upper field
+		if (height != 0) {
 			if (map[height--][width--].equals("|") && map[height -= 2][width].equals("-")
 					&& map[height--][width++].equals("|")) {
 				points += 1;
 			} else
 				points += 0;
+		}
 
-		} else if (height != engine.getHeight()) { // we can check lower field
+		// we can check lower field
+		if (height != (engine.getHeight() - 1)) {
 			if (map[height++][width--].equals("|") && map[height += 2][width].equals("-")
 					&& map[height++][width++].equals("|")) {
 				points += 1;
