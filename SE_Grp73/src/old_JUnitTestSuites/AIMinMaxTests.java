4fb8295ea73f27604c6d1a84e80c67a372fb96ee
Len Williamson <len13@hotmail.de>
2015-12-06 22:16:54 +0100

    Test Package hinzugefügt

---
+++ SE_Grp73/src/old_JUnitTestSuites/AIMinMaxTests.java
@@ -0,0 +1,14 @@
+package old_JUnitTestSuites;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AIMinMaxTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
